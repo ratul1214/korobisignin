f487fb37ee1fd9b2118382bcef2ce9dfdf2072a4
ratul1214 <alamalsaud5@gmail.com>
2020-01-01 10:36:22 +0600

    Add files via upload

---
+++ Authentication/Context/AuthenticationContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserAuthentication.Models;
+
+
+namespace UserAuthentication.Context
+{
+    public class AuthenticationContext: IdentityDbContext
+    {
+        public AuthenticationContext(DbContextOptions  options ):base(options)
+        {
+
+        }
+        public Microsoft.EntityFrameworkCore.DbSet<ApplicationUser> applicationUser { get; set; }
+    }
+}
---
+++ Authentication/Controllers/ApplicationUserController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UserAuthentication.Models;
+
+namespace UserAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationUserController : ControllerBase
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+        public ApplicationUserController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<Object>PostApplicationUser(ApplicationUserModel model){
+            var applicationser = new ApplicationUser()
+            {
+                UserName = model.UserName,
+                Email = model.Email,
+                FullName = model.FullName
+            };
+            try
+            {
+                var result = await _userManager.CreateAsync(applicationser, model.Password);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+    }
+}
